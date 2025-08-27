a30c49eb2bb1637830eb1dcac42c6f0f8ad555d4
k17vivek <uservk17@gmail.com>
2025-08-28 01:11:11 +0530

    Fix eslint config (fileURLToPath import error)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { dirname } from "path";
-import { fileNEXT_PUBLIC_URLToPath } from "url";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileNEXT_PUBLIC_URLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
